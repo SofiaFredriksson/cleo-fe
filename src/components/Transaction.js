5e443f148d7a93723573f71b0548f90c8d237554
SofiaFredriksson <sofiapkf@gmail.com>
2019-12-08 15:55:37 +0000

    styling - add box shadow to cards andbuttons, remove outline

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import "./Transaction.scss";
+import React, { Component } from "react"
+import "./Transaction.scss"
 
 export default class Transaction extends Component {
   render() {
-    const { amount, date } = this.props;
+    const { amount, date } = this.props
     return (
-      <div className="transaction-card">
+      <div className="card">
         <div className="card-body">
           <h4>{date}</h4>
           <p>Amount: {amount}</p>
         </div>
       </div>
-    );
+    )
   }
 }
